42af6ebc5046776fdc52b7a5abe2904b34ff7779
Ryder Mackay <Ryder@platypus.local>
2013-01-05 06:16:05 -0500

    Initial Commit

---
+++ Node/RGMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RGMAppDelegate.h
+//  Node
+//
+//  Created by Ryder Mackay on 2013-01-05.
+//  Copyright (c) 2013 Ryder Mackay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Node/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Node
+//
+//  Created by Ryder Mackay on 2013-01-05.
+//  Copyright (c) 2013 Ryder Mackay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RGMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RGMAppDelegate class]));
+    }
+}
